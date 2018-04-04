ffa17bcd98354f012a75adc92df410e0f672dc98
richelbilderbeek <richel@richelbilderbeek.nl>
2018-04-04 18:19:47 +0200

    EOD

--- issue_6.R
+++ issue_6.R
@@ -1,12 +1,14 @@
-ncols <- 1100
+ncols <- 1050
 nrows <- 40000
 ncells <- ncols*nrows
 testit::assert(ncells < 2^32-1)
 mem_use <- ncells * object.size(3.14)
-format(mem_use, units = "Gb")
+format(mem_use, units = "Mb")
 
 # Computer will freeze if you ignore this warning ..
-testit::assert(mem_use < 2^32-1)
+testit::assert(mem_use < ((2^32)-1))
+
+((2^32)-1) / as.double(object.size(3.14)) / ncols
 
 df <- data.frame(matrix(nrow = nrows, ncol = ncols, data = seq(1,ncells)))
 df[2,1:10]
